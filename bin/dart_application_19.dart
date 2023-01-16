26afd189f2009b18086d10f553a229abbbbe412e
riyasklb <riyasklb89@gmail.com>
2023-01-16 12:09:32 +0530

    first commit

---
+++ bin/dart_application_19.dart
@@ -0,0 +1,18 @@
+//import 'package:dart_application_19/dart_application_19.dart'
+// as dart_application_19;
+
+void main(List<String> arguments) {
+  var age = 11;
+  var result = age > 18 ? "h" : "l";
+  print(result);
+  int a = 6;
+  int b = 5;
+  int c = 7;
+  //var out = (a > b) ? a : b;
+  // print(out);
+  // var out1 = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+  // print(out1);
+  int? x;
+  var d = x ?? 50;
+  print(d);
+}
